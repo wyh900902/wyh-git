d0c53f753ae5251880d9a677d0eb9356ae6fa737
huige001 <445906339@qq.com>
2018-06-04 14:07:47 +0800

    init

---
+++ src/main/java/com/wyh/WyhGitApplication.java
@@ -0,0 +1,12 @@
+package com.wyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyhGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyhGitApplication.class, args);
+	}
+}
